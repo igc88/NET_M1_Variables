ea879b96f877621711ffb3bee1d04c07519bb221
Iago <iago.gc88@gmail.com>
2020-12-31 17:48:21 +0100

    MILESTONE2
    Casting variables.

---
+++ Classes/Milestone2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_M1 {
+    class Milestone2 {
+        public void init() {
+            double number = 1.2345;
+            float f = (float)number;
+            int i = (int)number;
+            string s=number.ToString();
+
+            Console.WriteLine("Float: {0}", f);
+            Console.WriteLine("Int: {0}", i);
+            Console.WriteLine("String: {0}", s);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 namespace NET_M1 {
     class Program {
         static void Main(string[] args) {
-            Milestone1 ex1 = new Milestone1();
+            //Milestone1 ex1 = new Milestone1();
             //ex1.fase1();
             //ex1.fase23();
-            ex1.fase4();
+            //ex1.fase4();
+
+            Milestone2 m2 = new Milestone2();
+            m2.init();
 
             Console.ReadKey();
         }
